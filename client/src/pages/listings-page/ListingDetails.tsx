2502a98d8b2eae3ca84feeca7d5aa4035a6ce899
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-03-13 23:58:29 -0400

    made few visual changes

--- client/src/pages/listings-page/ListingDetails.tsx
+++ client/src/pages/listings-page/ListingDetails.tsx
@@ -21,23 +21,47 @@ function ListingDetails() {
       <div className=" p-3 max-w-screen-xl w-full flex  flex-col items-center justify-center gap-6">
         <div className="flex flex-col gap-8 max-w-5xl ">
           <h1 className="text-3xl font-bold">{listingLocation}</h1>
-          <div className="flex gap-2 justify-center rounded-2xl overflow-hidden">
-            <div className="">
-              <img src={mainListingImageSrc} alt="FrontView" width="500" />
+          <div className="flex gap-2 justify-center rounded-2xl overflow-hidden h-[400px]">
+            <div className="w-1/2 h-full">
+              <img
+                src={mainListingImageSrc}
+                alt="FrontView"
+                className="h-full w-full object-cover"
+              />
             </div>
 
-            <div className="grid grid-cols-2 gap-2">
-              <img src={image1} alt="Interior1" width="240" />
-              <img src={image2} alt="Interior2" width="240" />
-              <img src={image3} alt="Interior3" width="240" />
-              <img src={image4} alt="Interior4" width="240" />
+            <div className="flex flex-col gap-2 w-1/2 justify-center h-full">
+              <div className="flex gap-2 h-1/2 w-full">
+                <img
+                  src={image1}
+                  alt="Interior1"
+                  className="w-1/2 h-full object-cover"
+                />
+                <img
+                  src={image2}
+                  alt="Interior2"
+                  className="w-1/2 h-full object-cover"
+                />
+              </div>
+              <div className="flex gap-2 h-1/2 w-full">
+                <img
+                  src={image3}
+                  alt="Interior3"
+                  className="w-1/2 h-full object-cover"
+                />
+                <img
+                  src={image4}
+                  alt="Interior4"
+                  className="w-1/2 h-full object-cover"
+                />
+              </div>
             </div>
           </div>
 
           <div className="flex justify-between">
             <div>
               <p className="font-bold">
-                {NoOfBed} beds · {NoOfBath} baths
+                {NoOfBed} bed(s) · {NoOfBath} bath(s)
               </p>
             </div>
             <div>

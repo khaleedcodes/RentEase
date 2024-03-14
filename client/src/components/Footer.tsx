5bed3d87bff96d2148c9b010fdafc3ea948ec40c
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-03-14 01:03:09 -0400

    designed sign up page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,17 +1,15 @@
-import { Link } from "react-router-dom";
-
 function Footer() {
   return (
     <div className=" flex items-center justify-center h-28 ">
       <div className="w-full max-w-7xl h-full border-t-2 flex items-center p-4">
         <p className="text-sm text-center">
           &#169; 2024 Rentease by{" "}
-          <Link
-            to="/team"
+          <a
+            href="/team"
             className="font-bold text-first-primary hover:text-first-primary-light transition-all duration-150 ease-in-out"
           >
             Bit Builders
-          </Link>
+          </a>
           .
         </p>
       </div>
--- client/src/pages/signup-page/SignupForm.tsx
+++
@@ -1,7 +0,0 @@
-function SignupForm() {
-  return (
-    <div>SignupForm</div>
-  )
-}
-
-export default SignupForm

4b61f610f785587540172c32ed0572c22d8e942e
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-03-16 11:54:45 -0400

    final

--- server/index.js
+++ server/index.js
@@ -16,30 +16,26 @@ app.get("/getListings", (req, res) => {
     .catch((err) => res.json(err));
 });
 
-const mongoURL =
-  "mongodb+srv://FrancescaGalang:zIxWkoXIOgieSd4B@cluster0.qywthgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// mongoose.connect(mongoURL);
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  RegisterModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("The password is incorrect");
+      }
+    } else {
+      res.json("No record found");
+    }
+  });
+});
 
-// app.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-//   RegisterModel.findOne({ email: email }).then((user) => {
-//     if (user) {
-//       if (user.password === password) {
-//         res.json("Success");
-//       } else {
-//         res.json("The password is incorrect");
-//       }
-//     } else {
-//       res.json("No record found");
-//     }
-//   });
-// });
-
-// app.post("/signup", (req, res) => {
-//   RegisterModel.create(req.body)
-//     .then((users) => res.json(users))
-//     .catch((err) => res.json(err));
-// });
+app.post("/signup", (req, res) => {
+  RegisterModel.create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
 
 app.listen(3001, () => {
   console.log("server is running on port 3001");

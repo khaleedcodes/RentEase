8d6c2f66a144eb719999b9be53b5c6492049e9db
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-03-27 15:56:28 -0400

    new modifications to teams page

--- client/src/pages/signup-page/SignupPage.tsx
+++ client/src/pages/signup-page/SignupPage.tsx
@@ -138,11 +138,13 @@ function Signup() {
                 name="phone"
                 onChange={(e) => setPhone(e.target.value)}
               />
-              <div className="flex items-center bg-first-secondary w-full min-h-11 rounded-md outline-none border-none justify-center gap-2 flex-wrap">
-                <IdentificationIcon />
-                <label htmlFor="email">
-                  <p className="text-[#71767b]">Valid Government ID Here:</p>
-                </label>
+              <div className="flex items-start pl-10 bg-first-secondary w-full min-h-11 rounded-md outline-none border-none justify-center gap-2 flex-wrap flex-col p-2">
+                <div className="flex gap-2 items-center justify-center">
+                  <IdentificationIcon />
+                  <label htmlFor="email">
+                    <p className="text-[#71767b]">Valid Government ID Here:</p>
+                  </label>
+                </div>
 
                 <input
                   accept="image/*"
